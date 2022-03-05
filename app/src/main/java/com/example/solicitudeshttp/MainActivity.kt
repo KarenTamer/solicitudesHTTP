46ba43f728402b553451fb912d4ff48ead5d882a
Karen Tamer <karen.poot@cbtis72.edu.mx>
2022-03-05 13:42:33 -0500

    Solicitudeshttp

--- app/src/main/java/com/example/solicitudeshttp/MainActivity.kt
+++ app/src/main/java/com/example/solicitudeshttp/MainActivity.kt
@@ -2,17 +2,65 @@ package com.example.solicitudeshttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val bValidarRed = findViewById<Button>(R.id.bValidarRed)
+        val bSolicitudHTTP = findViewById<Button>(R.id.bSolicitudHTTP)
+
         bValidarRed.setOnClickListener{
             //Código para validar red
+            if(Network.isNetworkAvailable( this)){
+                Toast.makeText(this,"Si hay red!",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this,"No hay una conexión a internet",Toast.LENGTH_LONG).show()
+            }
+        }
 
+        bSolicitudHTTP.setOnClickListener{
+            //Código para validar red
+            if(Network.isNetworkAvailable( this)){
+                Log.d("bSolicitudOnClick", descargarDatos( "http://www.google.com"))
+            }else{
+                Toast.makeText(this,"No hay una conexión a internet",Toast.LENGTH_LONG).show()
+            }
         }
     }
+
+        @Throws(IOException::class)
+        private fun descargarDatos(url:String):String{
+
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+
+            var inputStream: InputStream? = null
+
+        try {
+            val url = URL(url)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.requestMethod = "GET"
+            conn.connect()
+
+            inputStream = conn.inputStream
+            return  inputStream.bufferedReader().use {
+                it.readText()
+            }
+        }finally {
+            if (inputStream != null) {
+                inputStream.close()
+            }
+          }
+        }
 }
